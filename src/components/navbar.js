04a242415abd4c4759a1c24d68e63770ff71ef28
gabritonce <126726082+gabritonce@users.noreply.github.com>
2023-08-07 23:33:34 +0200

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="navba navbar-dark p-3" style={{ backgroundColor: "#457b9d", borderColor: "#457b9d" }}>
+    <nav className="navbar navbar-dark p-3" style={{ backgroundColor: "#457b9d", borderColor: "#457b9d" }}>
       <div className="container-fluid">
         <span className="navbar-brand mb-0 h1" style={{ fontSize: '20px', fontWeight: 'bold' }}>FullStack Sushi</span>
       </div>
